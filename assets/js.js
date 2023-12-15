b2524202345f24fc36c92f0c134f029350981ccf
irebm2 <irebazaga@gmail.com>
2023-12-15 20:50:25 +0000

    oki

--- assets/js.js
+++ assets/js.js
@@ -1,10 +1,12 @@
-$(document).ready(function() {
-    // Array to store the searched cities
-    var history = [];
+$(document).ready(function () {
+    // Attach the form submission handler to the search form
+    $('#search-form').on('submit', handleFormSubmit);
+  
+    let history = []; // Create an array to store the city history
   
     function fetchWeatherData(city) {
-      var apikey = '1182b819fb971825022b5fab780f5857';
-      var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(city)},uk&appid=${apikey}`;
+      const apikey = '1182b819fb971825022b5fab780f5857';
+      const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(city)},uk&appid=${apikey}`;
   
       fetch(queryURL)
         .then(response => response.json())
@@ -18,59 +20,70 @@ $(document).ready(function() {
     function handleFormSubmit(event) {
       event.preventDefault();
   
-      var city = $('#search-input').val();
+      const city = $('#search-input').val();
   
-      // Check if the city exists in the history array
-      if (!history.includes(city)) {
-        // Add the city to the history array
-        history.push(city);
+      // Save the city name to a variable before checking the history array
+      const storedCity = city;
   
-        // Create a history button for the new city
-        createHistoryButton(city);
+      // Remove any existing buttons with the same city name
+      $('.history-button').each(function () {
+        if ($(this).text() === storedCity) {
+          $(this).remove();
+        }
+      });
+  
+      // Check if the city is already in history
+      if (!history.includes(storedCity)) {
+        // Create the history button
+        createHistoryButton(storedCity);
       } else {
-        console.log('Skipping adding ' + city + ' to the history, since it is already present.');
+        console.log('Skipping creating a history button for ' + storedCity + ', since it is already in the history.');
       }
   
-      // Clear the input value after submission
+      // Optional: Reset the input value after submission
       $('#search-input').val('');
     }
   
     function createHistoryButton(city) {
-      var capitalizedCity = capitalizeFirstLetter(city);
+      const capitalizedCity = capitalizeFirstLetter(city);
   
       // Check if the city is in the history array
       if (!history.includes(capitalizedCity)) {
         // Create the button and add it to the history list
-        var button = $('<button type="button"></button>');
+        const button = $('<button type="button"></button>');
         button.text(capitalizedCity);
         button.addClass('history-button');
         button.data('city', capitalizedCity);
   
-        var historyList = $('#history');
+        const historyList = $('#history');
         historyList.append(button);
+  
+        // Add the city to the history array
+        history.push(capitalizedCity);
       }
     }
   
     // Helper function to capitalize the first letter of a string
-  
-    
-  function
-   
-  capitalizeFirstLetter(string) {
+    function capitalizeFirstLetter(string) {
       return string.charAt(0).toUpperCase() + string.slice(1);
     }
   
-    var removeButton = $('<button class="remove-button btn"> Clear history </button>');
-    var inputGroup = $('.input-group');
+    const removeButton = $('<button class="remove-button btn"> Clear history </button>');
+    const inputGroup = $('.input-group');
     inputGroup.append(removeButton);
   
-    removeButton.on('click', function() {
-      // Empties the history array
+    removeButton.on('click', function () {
+      // Remove all child elements with the class "history-button" from the "history" container
+      $('.history-button').remove();
+  
+      // Clear the history array
       history = [];
   
-      // Remove all child elements with the class "history-button" from the "history" container
-      $('#history').children('.history-button').remove();
+      // Optional: Create a history button for the previously searched city, even if it was listed before
+      createHistoryButton(storedCity);
     });
   
-    var searchBar = $('#search-input');
+    const searchBar = $('#search-input');
+    const cityName = $('<h2 class="city-name">' + capitalizedCity + '</h2>');
+    searchBar.closest('.input-group').append(cityName);
   });
